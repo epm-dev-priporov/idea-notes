416f35d8e12f1537183bf010b99e57a754fa687b
Dmitriy_Priporov <Dmitriy.Priporov@bia-tech.ru>
2022-10-31 23:12:06 +0300

    0.9.2/added markdown and puml files

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.priporov"
-version = "0.9.1"
+version = "0.9.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/dev/priporov/ideanotes/tree/common/ExtensionFileHelper.kt
+++ src/main/kotlin/dev/priporov/ideanotes/tree/common/ExtensionFileHelper.kt
@@ -8,17 +8,35 @@ import javax.swing.Icon
 class ExtensionFileHelper {
     companion object {
         val EXTENSIONS: MutableMap<String, ExtensionData> = sequenceOf(
-            ExtensionData("txt", "Text node", "icons8-file-16.png", "icons-files-16.png"),
-            ExtensionData("json", "Json node", "json/json16.png", "json/json16.png"),
-            ExtensionData("xml", "Xml node", "yaml/yaml16.png", "yaml/yaml16.png"),
-            ExtensionData("yaml", "Yaml node", "xml/xml16.png", "xml/xml16.png"),
-            ExtensionData("sql", "Sql node", "sql/sql16.png", "sql/sql16.png"),
+                ExtensionData("txt", "Text node", "icons8-file-16.png", "icons-files-16.png"),
+                ExtensionData("json", "Json node", "json/json16.png", "json/json16.png"),
+                ExtensionData("xml", "Xml node", "yaml/yaml16.png", "yaml/yaml16.png"),
+                ExtensionData("yaml", "Yaml node", "xml/xml16.png", "xml/xml16.png"),
+                ExtensionData("sql", "Sql node", "sql/sql16.png", "sql/sql16.png"),
         ).associateByTo(HashMap()) { it.extension }
 
         init {
-            val pluginId: PluginId? = PluginId.findId("com.jetbrains.restClient")
+            sequenceOf(
+                    PluginDependency(
+                            "org.intellij.plugins.markdown",
+                            ExtensionData("md", "Markdown node", "md/markdown16.png", "md/markdown16.png"),
+                    ),
+                    PluginDependency(
+                            "PlantUML integration",
+                            ExtensionData("puml", "Puml node", "puml/puml16.png", "puml/puml16.png"),
+                    ),
+                    PluginDependency(
+                            "com.jetbrains.restClient",
+                            ExtensionData("http", "Http node", "http/http16.png", "http/http16.png")
+                    ),
+            ).forEach { applyExtension(it) }
+        }
+
+        private fun applyExtension(pluginDependency: PluginDependency) {
+            val pluginId: PluginId? = PluginId.findId(pluginDependency.id)
             if (pluginId != null && PluginManager.getInstance().findEnabledPlugin(pluginId) != null) {
-                EXTENSIONS["http"] = ExtensionData("http", "Http node", "http/http16.png", "http/http16.png")
+                val data = pluginDependency.extendionData
+                EXTENSIONS[data.extension] = data
             }
         }
 
@@ -26,11 +44,16 @@ class ExtensionFileHelper {
 
 }
 
+class PluginDependency(
+        val id: String,
+        val extendionData: ExtensionData
+)
+
 class ExtensionData(
-    val extension: String,
-    val definition: String,
-    leafIconPath: String,
-    nodeIconPath: String,
-    val leafIcon: Icon = IconUtils.toIcon(leafIconPath),
-    val nodeIcon: Icon = IconUtils.toIcon(nodeIconPath),
+        val extension: String,
+        val definition: String,
+        leafIconPath: String,
+        nodeIconPath: String,
+        val leafIcon: Icon = IconUtils.toIcon(leafIconPath),
+        val nodeIcon: Icon = IconUtils.toIcon(nodeIconPath),
 )
