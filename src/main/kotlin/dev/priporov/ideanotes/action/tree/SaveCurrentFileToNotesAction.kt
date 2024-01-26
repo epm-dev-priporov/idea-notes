da98dcf4f59403eb4200a6d9568c3a7450102513
Dmitrii_Priporov <devpriporov@mail.ru>
2024-01-26 11:50:31 +0300

    1.4.0/wip over SaveCurrentFileToNotesAction

---
+++ src/main/kotlin/dev/priporov/ideanotes/action/tree/SaveCurrentFileToNotesAction.kt
@@ -0,0 +1,10 @@
+package dev.priporov.ideanotes.action.tree
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class SaveCurrentFileToNotesAction: AnAction("Copy to Notes") {
+    override fun actionPerformed(p0: AnActionEvent) {
+
+    }
+}
