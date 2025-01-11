a8e83a6172347a5c3a106da1a29f61d943ef1a72
Dmitrii_Priporov <devpriporov@mail.ru>
2025-01-11 16:40:48 +0300

    2.0.0 wip over extensions

--- src/main/kotlin/dev/priporov/ideanotes/tree/node/init/NodeDefinition.kt
+++ src/main/kotlin/dev/priporov/ideanotes/tree/node/init/NodeDefinition.kt
@@ -1,9 +1,7 @@
 package dev.priporov.ideanotes.tree.node.init
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.registry.Registry
-import dev.priporov.ideanotes.icon.Icons
 import javax.swing.Icon
 
 
@@ -13,12 +11,13 @@ class NodeDefinition(
     @JsonProperty("extension") var extension: String,
     @JsonProperty("definition") var definition: String,
     @JsonProperty("plugin") var plugin: String? = null,
+    @JsonProperty("ide") var listOfSUpportedIde: List<String>? = null,
+    @JsonProperty("os") var os: List<String>? = null,
 ) {
     private var icon: Icon? = type.icon
     private var newUiIcon: Icon? = type.newUiIcon
 
     fun getRequiredIcon() = if (isNewUi()) newUiIcon else icon
-
 
     companion object {
         fun isNewUi() = Registry.`is`("ide.experimental.ui")
--- src/main/kotlin/dev/priporov/ideanotes/tree/node/init/NodeDefinitionService.kt
+++ src/main/kotlin/dev/priporov/ideanotes/tree/node/init/NodeDefinitionService.kt
@@ -1,16 +1,43 @@
 package dev.priporov.ideanotes.tree.node.init
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.components.Service
+import java.util.*
 
 @Service
 class NodeDefinitionService {
 
-    lateinit var mapOfNodeDefinitions: HashMap<NodeType, NodeDefinition>
+    lateinit var mapOfNodeDefinitions: Map<NodeType, NodeDefinition>
     lateinit var orderedNodeDefinitions: List<NodeDefinition>
 
-    fun init(nodeDefinitions: java.util.HashMap<NodeType, NodeDefinition>) {
+    private val ideName = ApplicationInfo.getInstance().fullApplicationName
+    private val osName = System.getProperty("os.name").lowercase(Locale.ENGLISH)
+
+    fun init(nodeDefinitions: Map<NodeType, NodeDefinition>) {
         this.mapOfNodeDefinitions = nodeDefinitions
-        this.orderedNodeDefinitions = nodeDefinitions.values.sortedBy { it.index }
+        this.orderedNodeDefinitions = nodeDefinitions
+            .values
+            .asSequence()
+            .filter(this::filterByOs)
+            .filter(this::filterByIde)
+            .sortedBy { it.index }
+            .toList()
     }
 
+    private fun filterByIde(definition: NodeDefinition): Boolean {
+        val ides = definition.listOfSUpportedIde
+        if (!ides.isNullOrEmpty()) {
+            return ides.any { name -> ideName.contains(name) }
+        }
+        return true
+    }
+
+    private fun filterByOs(definition: NodeDefinition): Boolean {
+        val os = definition.os
+
+        if (!os.isNullOrEmpty()) {
+            return os.any { name -> osName.contains(name) }
+        }
+        return true
+    }
 }
