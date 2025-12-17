2f86e7641a9b435d7dff9bdac3d38477ff29ca0f
Dmitrii_Priporov <devpriporov@mail.ru>
2025-12-17 23:09:51 +0300

    1.5.8 up to 253.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.priporov"
-version = "1.5.7"
+version = "1.5.8"
 
 repositories {
     mavenCentral()
@@ -31,16 +31,16 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231")
-        untilBuild.set("252.*")
+        untilBuild.set("253.*")
     }
 
     signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+        certificateChain.set(providers.environmentVariable("CERTIFICATE_CHAIN"))
+        privateKey.set(providers.environmentVariable("PRIVATE_KEY"))
+        password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
     }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+//    publishPlugin {
+//        token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+//    }
 }
